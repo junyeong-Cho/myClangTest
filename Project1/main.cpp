5cbf06417e3cac0285c651e8f2a7c4a9c6efc2b0
Junyeong-Cho <tpqldj1312@gmail.com>
2023-09-19 13:10:39 +0900

    clang and CI test complete

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 
-
-
-class myName
-{
-
-};
-
-
+class myName {};
 
 int abcesesdf = 4;
 int asdf = 4;
 int asd = 1;
 
+int main() {
+  std::cout << "Hello my name is Junyeong Cho" << std::endl;
 
-int main()
-{
-	std::cout << "Hello my name is Junyeong Cho" << std::endl;
+  while (false) {
+  }
 
-	while (false) {}
-    while (true) 
-	{
-      break;
-	}
+  while (true) {
 
-
+    break;
+  }
 }
