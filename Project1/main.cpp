0a38ec1f78c8c579ce4cad0cd69e99cc0ad96b87
Junyeong-Cho <tpqldj1312@gmail.com>
2023-09-19 12:37:08 +0900

    clang-format test start

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
+
+
 class myName
 {
 
 };
 
 
+
+int abcesesdf = 4;
+int asdf = 4;
+int asd = 1;
+
+
 int main()
 {
 	std::cout << "Hello my name is Junyeong Cho" << std::endl;
+
+	while (false) {}
+    while (true) 
+	{
+      break;
+	}
+
+
 }
