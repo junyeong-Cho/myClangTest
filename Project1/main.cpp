bcd5dc8441c0bf63d2ad9507bc5c23018ccf3f3d
junyeong-Cho <103232246+junyeong-Cho@users.noreply.github.com>
2023-09-21 17:29:27 +0900

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,18 +3,15 @@
 class myName {};
 
 int abcesesdf = 4;
-int asdf               = 4;
+int asdf = 4;
 int asd = 1;
+
 
 int main() {
   std::cout << "Hello my name is Junyeong Cho" ;
 
   while (false) 
-  
-  
- {
-
-
+    {
 
   }
 
